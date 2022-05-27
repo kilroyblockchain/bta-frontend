a1456abbf788ae1b3cc9bec72124ae54a4f642ae
surya <surya@kilroyblockchain.com>
2022-05-27 16:24:46 +0545

    [BB-4]: removed unnecessary environment

--- src/environments/environment.obc.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    project: 'flo',
-    internationalization: 'enabled',
-    apiURL: 'https://obc-api.caseyhealth.net/api/v1',
-    hostURL: 'https://obc.caseyhealth.net/#/',
-    resultsPerPage: 15
-};
