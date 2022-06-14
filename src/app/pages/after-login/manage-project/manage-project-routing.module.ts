3bf633819ea5acddf941dcdcc7da3ad9be14bf70
surajthaqurie <surajchan68@gmail.com>
2022-06-14 16:17:58 +0545

    [BB-26]: remove unusable project guard

--- src/app/pages/after-login/manage-project/manage-project-routing.module.ts
+++ src/app/pages/after-login/manage-project/manage-project-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectGuard } from 'src/app/@core/guard/project.guard';
 import { ChannelSetUpComponent } from './manage-channel/manage-channel.component';
 import { ManageProjectComponent } from './manage-project.component';
 import { ProjectComponent } from './project/project.component';
@@ -9,7 +8,6 @@ const routes: Routes = [
     {
         path: '',
         component: ManageProjectComponent,
-        canActivate: [ProjectGuard],
         children: [
             {
                 path: 'all',
