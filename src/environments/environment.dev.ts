6a4b7e19de23368fe34abb76f388ef8051a6f62a
Suyog Khanal <Suyogkh@gmail.com>
2022-08-17 13:29:03 +1000

    Base configuration updated

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    project: 'base-proj',
+    project: 'bta',
     production: true,
     apiURL: 'https://dev-api.baseproj.net/api/v1',
     hostURL: 'https://dev-api.baseproj.net/#/',
     internationalization: 'enabled',
     resultsPerPage: 15,
     disableCaptcha: false,
-    recaptchaSiteKey: '6LdKDNYfAAAAAHIhducYusgXcLHj_-3xF4qAesXZ'
+    recaptchaSiteKey: '6Leox4EhAAAAACUbw7R68nrnP1vmCVt8ELRi4Bs0'
 };
